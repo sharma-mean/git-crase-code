dfdebdadec1e83d1eba61c197f6d86c3afceece9
Vikash Sharma <sharmavikash74497@gmail.com>
2024-07-28 23:14:07 +0530

    new featurs

---
+++ index.js
@@ -0,0 +1,11 @@
+const x = 10;
+const name = "Viaksh";
+
+const ph = 12345678;
+
+const age =123;
+
+const details =()=>{
+    console.log(`my name is ${name},  ph is ${ph} and age is ${age}`)
+}
+console.log(`my name is ${ph}`)
---
+++ utility/utility.js
@@ -0,0 +1,4 @@
+const fullName = (fname, lname)=>{
+    return `fname ${fname} and ${lname}` 
+}
+console.log(fullName());
